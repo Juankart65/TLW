13ccf812e7d583c49d05cc6a8c82d50b57b5d90b
Juankart65 <104481229+Juankart65@users.noreply.github.com>
2023-11-14 21:12:57 -0500

    Update

--- client/src/pages/UsoH.jsx
+++ client/src/pages/UsoH.jsx
@@ -12,43 +12,58 @@ export default function H() {
         <h2 className='text-xl md:text-5xl flex-col mb-6 md:mb-10 text-center font-serif '><b><i>Uso de la |H|</i></b></h2>
       </div>
 
-      {/* <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
-        <p className='text-slate-900 text-center font-serif'><i>La letra <b>J</b> en el español desempeña un papel crucial en la
-          formación de palabras y enriquece la diversidad fonética del idioma. Es una consonante que se utiliza con frecuencia
-          y que se encuentra en diversas posiciones dentro de las palabras, aportando matices y sonidos particulares.
-          A continuación, se explorará el uso de la letra <b>J</b> en el idioma español, conectando conceptos y destacando
-          sus múltiples funciones.</i></p>
+      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
+        <p className='text-slate-900 text-center font-serif'><i>Se escriben con H inicial todas las palabras que empiezan por los
+          distintos ia, ie, ue o ui.</i></p>
+      </div>
+
+      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
+        <p className='text-slate-900 text-center font-serif'><i>Se escriben con H inicial las formas de los verbos haber, hace,
+          hablar, halla y habitar. Ejemplos: <b>● hay  ● hacían  ● habló ● hallará  ● habitan</b></i></p>
       </div>
       <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
-        <p className='text-slate-900 text-center font-serif'><i>Primero, la J se encuentra comúnmente al principio
-          de palabras, proporcionando un sonido fuerte y distintivo. Ejemplos paradigmáticos incluyen <b>joven</b>, <b>justicia</b> y <b>jirafa</b>.
-          Este rasgo inicial conecta estas palabras bajo el patrón fonético característico de la <b>J</b> en el español.</i></p>
-      </div>
-      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
-        <p className='text-slate-900 text-center font-serif'><i>Además, la <b>J</b> también se utiliza en combinación con otras
-          letras, como en los casos de <b>je</b> y <b>ji</b>. Estas combinaciones refuerzan la diversidad fonética del español,
-          añadiendo sonidos que no se encuentran en otras lenguas romances. Este fenómeno, conocido como la <b>ge</b> y la <b>gi</b> suave,
-          es un elemento distintivo del español que se conecta con su evolución lingüística a lo largo del tiempo.</i></p>
-      </div>
-      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
-        <p className='text-slate-900 text-center font-serif'><i>En términos de gramática, la <b>j</b> puede desempeñar un papel en la
-          conjugación de verbos. Por ejemplo, en los verbos que terminan en <b>-cir</b> como <b>producir</b> o <b>conducir</b>, la <b>j</b> se convierte
-          en una letra fundamental en la formación de diversas formas verbales. Esta conexión gramatical destaca la versatilidad de
-          la <b>j</b> en la estructura de las palabras y su impacto en la comunicación efectiva.</i></p>
+        <p className='text-slate-900 text-center font-serif'><i>Se escriben con H las palabras que empiezan por hum. Ejemplo: <b>● húmeda
+          ● humano ● humo  ● humilde ● humor</b></i></p>
       </div>
 
       <div className='flex flex-col items-center justify-center w-full'>
-        <h2 className='text-xl md:text-5xl flex-col mb-6  mt-5 md:mb-10 text-center font-serif '><b><i>Ejemplos</i></b></h2>
+        <h2 className='text-xl md:text-5xl flex-col mt-6 mb-3 md:mb-3 text-center font-serif '><b><i>Palabras homófonas</i></b></h2>
+      </div>
+      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
+        <p className='text-slate-900 text-center font-serif'><i>Los términos homófonos son aquellos que suenan igual,
+          pero tienen distinto significado y pueden tener distinta grafía.</i></p>
+      </div>
+       <div className='flex flex-row justify-center items-center mb-10'>
+        <img src="https://res.cloudinary.com/dsnq0pvey/image/upload/f_auto,q_auto/v1/MundoLiterario/i3j68i6ombqkyq8bz3nu"
+          className='w-2/2 md:w-2/3' />
+      </div>
+      <div className='flex flex-col items-center justify-center w-full'>
+        <h2 className='text-xl md:text-5xl flex-col mt-6 mb-3 md:mb-3 text-center font-serif '><b><i>Interjecciones</i></b></h2>
       </div>
 
-       <div className='flex flex-row justify-center items-center mt-7 mb-10'>
-        <img src="https://res.cloudinary.com/dsnq0pvey/image/upload/f_auto,q_auto/v1/MundoLiterario/hrc83ntegbyomrsabjq1"
-          className='w-2/2 md:w-2/3' />
+      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
+        <p className='text-slate-900 text-center font-serif'><i>Siguiendo con las interjecciones, la h está presente en algunas de
+          las más usadas:</i></p>
       </div>
-       <div className='flex flex-row justify-center items-center mt-7 mb-10'>
-        <img src="https://res.cloudinary.com/dsnq0pvey/image/upload/f_auto,q_auto/v1/MundoLiterario/qzuxnw5lkbnh6fnfpsr0"
-          className='w-2/2 md:w-2/3' />
-      </div> */}
+      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
+        <p className='text-slate-900 text-center font-serif'><i><b>¡ah!:</b> para denotar pena, admiración, sorpresa o sentimientos
+          similares.</i></p>
+      </div>
+      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
+        <p className='text-slate-900 text-center font-serif'><i><b>¡eh!:</b> para preguntar, llamar, despreciar, reprender o advertir.</i></p>
+      </div>
+      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
+        <p className='text-slate-900 text-center font-serif'><i><b>¡oh!:</b> para manifestar muchos y muy diversos movimientos del ánimo,
+          y más ordinariamente asombro, pena o alegría.</i></p>
+      </div>
+      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 md:mr-6 md:ml-6'>
+        <p className='text-slate-900 text-center font-serif'><i><b>¡hala!:</b> para infundir aliento o meter prisa, para mostrar sorpresa
+          <b> (también puede escribirse ala y alá)</b>.</i></p>
+      </div>
+      <div className='text-small md:text-2xl mt-6 mr-4 ml-4 mb-6 md:mr-6 md:ml-6'>
+        <p className='text-slate-900 text-center font-serif'><i><b>¡huy!:</b> para denotar dolor físico agudo, vergüenza o asombro
+          <b> (también puede escribirse sin h, uy)</b>.</i></p>
+      </div>
 
     </div>
   )
