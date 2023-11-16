7756512ab2f64258f257b36922509ab97eacf7c4
Juankart65 <104481229+Juankart65@users.noreply.github.com>
2023-11-15 21:18:26 -0500

    update

--- client/src/pages/Bibliografias.jsx
+++ client/src/pages/Bibliografias.jsx
@@ -14,47 +14,47 @@ export default function Justificacion() {
       <div className='citation'>
         <p>Corte, J. L. (2005). <span className='cite'>Ortografía Práctica.</span> Bibliográfica Internacional.</p>
       </div>
-      {/* Cita 1 */}
+      {/* Cita 2 */}
       <div className='citation'>
         <p>Equipo Editorial, E. (16 de Julio de 2021). Concepto De. <span className='cite'>Palabras Homográfas</span> Obtenido de: <a href="https://concepto.de/palabras-homografas/" className='link'>https://concepto.de/palabras-homografas/</a> </p>
       </div>
-      {/* Cita 2 */}
+      {/* Cita 3 */}
       <div className='citation'>
         <p>FundéuRAE, E. b. (29 de Mayo de 2019). El blog de la FundéuRAE. <span className='cite'>Reglas de ortografía de la «h».</span> Obtenido de: <a href="https://www.fundeu.es/blog/reglas-de-ortografia-de-la-h/" className='link'>https://www.fundeu.es/blog/reglas-de-ortografia-de-la-h/</a></p>
       </div>
 
-      {/* Cita 3 */}
+      {/* Cita 4 */}
       <div className='citation'>
         <p>FundéuRAE, E. b. (17 de Mayo de 2019). El blog de la FundéuRAE. <span className='cite'>Reglas de ortografía de la «g» y la «j»</span> Obtenido de: <a href="https://www.fundeu.es/blog/reglas-de-ortografia-de-la-g-y-la-j/" className='link'>https://www.fundeu.es/blog/reglas-de-ortografia-de-la-g-y-la-j/</a></p>
       </div>
 
-      {/* Cita 4 */}
+      {/* Cita 5 */}
       <div className='citation'>
         <p>Giani, C. (24 de Diciembre de 2022). Enciclopedia de Ejemplos. <span className='cite'>Uso de la Y. </span> Obtenido de: <a href="https://www.ejemplos.co/uso-de-la-y/" className='link'>https://www.ejemplos.co/uso-de-la-y/</a></p>
       </div>
-      {/* Cita 5 */}
+      {/* Cita 6 */}
       <div className='citation'>
         <p>Giani, C. (7 de Agosto de 2022). Enciclopedia de Ejemplos. <span className='cite'>Uso de la X. </span> Obtenido de: <a href="https://www.ejemplos.co/uso-de-la-x/" className='link'>https://www.ejemplos.co/uso-de-la-x/</a></p>
       </div>
-      {/* Cita 6 */}
+      {/* Cita 7 */}
       <div className='citation'>
         <p>Madrid, U. C. (s.f.). Universidad Complutense de Madrid. <span className='cite'>Juan Luis Fuentes Gómez-Calcerrada</span> Obtenido de: <a href="https://educacion.ucm.es/juan-luis-fuentes-gomez-calcerrada" className='link'>https://educacion.ucm.es/juan-luis-fuentes-gomez-calcerrada</a></p>
       </div>
-      {/* Cita 7 */}
+      {/* Cita 8 */}
       <div className='citation'>
         <p>PNTIC. (s.f.). Uso Correcto de la LL. Obtenido de: <a href="http://descargas.pntic.mec.es/recursos_educativos/It_didac/Leng_Pri/2/05/01_uso_correcto_LL/" className='link'>http://descargas.pntic.mec.es/recursos_educativos/It_didac/Leng_Pri/2/05/01_uso_correcto_LL/</a></p>
       </div>
-      {/* Cita 8 */}
+      {/* Cita 9 */}
       <div className='citation'>
-        <p>RAE. (s.f.). Real Academia española. <span className='cite'>Uso de la W. </span> Obtenido de: <a href="https://www.rae.es/dpd/w" className='limk'>https://www.rae.es/dpd/w</a></p>
+        <p>RAE. (s.f.). Real Academia española. <span className='cite'>Uso de la W. </span> Obtenido de: <a href="https://www.rae.es/dpd/w" className='link'>https://www.rae.es/dpd/w</a></p>
       </div>
 
-      {/* Cita 9 */}
+      {/* Cita 10 */}
       <div className='citation'>
         <p>UniBetas. (29 de abril de 2022). unibetas. <span className='cite'>Uso de la h: Reglas, ejemplos y ejercicios resueltos.</span> Obtenido de: <a href="https://unibetas.com/uso-de-la-h" className='link'>https://unibetas.com/uso-de-la-h</a></p>
       </div>
 
-      {/* Cita 10 */}
+      {/* Cita 11 */}
       <div className='citation'>
         <p>Wited. (13 de Abril de 2020). Wited. <span className='cite'>Uso de la H.</span> Obtenido de: <a href="https://www.portaleducativo.net/cuarto-basico/751/Uso-de-la-H#:~:text=La%20H%20es%20una%20letra,%2C%20hablar%2C%20hallar%20y%20habitar." className='link'>https://www.portaleducativo.net/cuarto-basico/751/Uso-de-la-H/</a></p>
       </div>
